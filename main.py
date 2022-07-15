6269a1d5125936ddc39d12f5bc16c79b9d784957
Muhammad Rehan <rehan@revolveai.com>
2022-07-15 16:10:56 +0500

    Tracking time tweaking.

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def main():
     capture = cv2.VideoCapture(RTSP_STREAM_1)
 
     # Initialize MotPy tracker
-    tracker = MultiObjectTracker(dt=0.1)
+    tracker = MultiObjectTracker(dt=0.5)
 
     while True:
         try:
