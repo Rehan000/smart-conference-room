92c3ad0caf56cf54d8e6e866f830e0858212db18
Muhammad Rehan <rehan@revolveai.com>
2022-07-22 12:44:13 +0500

    Additions in pose estimation functionality.

--- pose_estimation.py
+++ pose_estimation.py
@@ -3,6 +3,28 @@ import cv2
 import sys
 import time
 import mediapipe as mp
+import numpy as np
+
+
+# Function to resize image while preserving aspect ratio
+def image_resize_aspect(img, newWidth):
+    old_size = img.shape
+    desired_size = newWidth
+    if old_size[0] < desired_size and old_size[1] < desired_size:
+        new_im = np.zeros([desired_size, desired_size, 3])
+        y = (desired_size - old_size[0]) // 2
+        x = (desired_size - old_size[1]) // 2
+        new_im[y:y + old_size[0], x:x + old_size[1]] = img
+    else:
+        ratio = float(desired_size) / max(old_size)
+        new_size = tuple([int(x * ratio) for x in old_size])
+        new_size = (new_size[1], new_size[0])
+        img = cv2.resize(img, new_size)
+        new_im = np.zeros([desired_size, desired_size, 3])
+        y = (desired_size - new_size[1]) // 2
+        x = (desired_size - new_size[0]) // 2
+        new_im[y:y + new_size[1], x:x + new_size[0]] = img
+    return np.uint8(new_im)
 
 
 # Main function
@@ -19,7 +41,11 @@ def main():
                 if status:
                     frame_fullsize_RGB = cv2.cvtColor(frame_fullsize, cv2.COLOR_BGR2RGB)
                     pose_results = pose.process(frame_fullsize_RGB)
-                    # cv2.imshow("Camera Stream", frame_fullsize_RGB)
+                    print(pose_results.pose_landmarks)
+                    frame_fullsize_BGR = cv2.cvtColor(frame_fullsize_RGB, cv2.COLOR_RGB2BGR)
+                    frame_resized = image_resize_aspect(frame_fullsize_BGR, 1280)
+                    frame_show = frame_resized[250:1030, 0:1280]
+                    # cv2.imshow("Camera Stream", frame_show)
                     # cv2.waitKey(1)
             else:
                 print("Camera Stream Issue!")
